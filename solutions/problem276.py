5626b9fa96f3d9c825fe81b17bcfdb9e07099fdf
Nikhi <nikhibhambra@gmail.com>
2020-11-23 19:31:19 -0500

    finished complexity comments

--- solutions/problem276.py
+++ solutions/problem276.py
@@ -17,20 +17,23 @@ def search(pattern: str, target: str) -> bool:
     Otherwise returns False.
     Assumptions: len(pattern) < len(target)
     """
-    # N
+    # 0 <= i <= N-1 iterations
     for i in range(len(target)):
         # whatever i ends up being, worst case i = n
         while i < len(target):
-            chunk = target[i:i+len(pattern)] # unnecessary, just to see iterations
-            print(chunk)
-            if chunk == pattern:
+            if target[i:i+len(pattern)] == pattern:
+                print(f"took {i+1} iterations, matched {target[i:i+len(pattern)]}")
                 return True
             i += 1
     x = target[-2:] == pattern
 
     # O(N * i) + 1 ?< O(N * k)
+    print(f"Took {len(target)} iterations, no match")
     return x or False
 
 if __name__ == "__main__":
     a = search("ik", "nikhi") # i iterates twice, so 3 iterations < 5*2 iterations
     print(a) #True
+
+    b = search("as", "nikhi") # i iterates 5 times, 5 < 5*2 iterations
+    print(b)
